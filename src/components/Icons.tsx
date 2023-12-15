2d63a925e47ed4d2a33de37e5125b21ad15048b7
tinotendajoe01 <tinotendajoe01@gmail.com>
2023-12-15 17:35:33 +0200

    first commit

---
+++ src/components/Icons.tsx
@@ -0,0 +1,6 @@
+import { LucideProps, User } from "lucide-react";
+
+export const Icons = {
+  user: User,
+  logo: (props: LucideProps) => <img src="/logos/logo-bw-bg.png" />,
+};
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,95 @@
+import Link from "next/link";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import { buttonVariants } from "./ui/button";
+import {
+  LoginLink,
+  RegisterLink,
+  getKindeServerSession,
+} from "@kinde-oss/kinde-auth-nextjs/server";
+import { ArrowRight } from "lucide-react";
+import UserAccountNav from "./UserAccountNav";
+import MobileNav from "./MobileNav";
+import Image from "next/image";
+
+const Navbar = () => {
+  const { getUser } = getKindeServerSession();
+  const user = getUser();
+
+  return (
+    <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
+      <MaxWidthWrapper>
+        <div className="flex h-14 items-center justify-between border-b border-zinc-200">
+          <Link href="/" className="flex  items-center z-40 font-semibold">
+            <Image
+              src="/logos/logo-green-bg-copy.png"
+              alt="product preview"
+              width={20}
+              height={20}
+              quality={100}
+              className="rounded-full"
+            />
+            <span>Brite</span> <span className="text-green-600">Ai.</span>
+          </Link>
+
+          <MobileNav isAuth={!!user} />
+
+          <div className="hidden items-center space-x-4 sm:flex">
+            {!user ? (
+              <>
+                <Link
+                  href="/pricing"
+                  className={buttonVariants({
+                    variant: "ghost",
+                    size: "sm",
+                  })}
+                >
+                  Pricing
+                </Link>
+                <LoginLink
+                  className={buttonVariants({
+                    variant: "ghost",
+                    size: "sm",
+                  })}
+                >
+                  Sign in
+                </LoginLink>
+                <RegisterLink
+                  className={buttonVariants({
+                    size: "sm",
+                    variant: "secondary",
+                  })}
+                >
+                  Get started <ArrowRight className="ml-1.5 h-5 w-5" />
+                </RegisterLink>
+              </>
+            ) : (
+              <>
+                <Link
+                  href="/dashboard"
+                  className={buttonVariants({
+                    variant: "ghost",
+                    size: "sm",
+                  })}
+                >
+                  Dashboard
+                </Link>
+
+                <UserAccountNav
+                  name={
+                    !user.given_name || !user.family_name
+                      ? "Your Account"
+                      : `${user.given_name} ${user.family_name}`
+                  }
+                  email={user.email ?? ""}
+                  imageUrl={user.picture ?? ""}
+                />
+              </>
+            )}
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </nav>
+  );
+};
+
+export default Navbar;
