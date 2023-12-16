4bba2712cacf099394c7cca696f573172845ea87
tinotendajoe01 <tinotendajoe01@gmail.com>
2023-12-16 16:57:34 +0200

    push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,18 +8,13 @@ export default function Home() {
   return (
     <>
       <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
-        {/* <div className="flex flex-col items-center justify-center rounded-md bg-white shadow-2xl ring-1 ring-gray-900/10 ">
-          <Image
-            src="/logos/logo-green-bg-copy.png"
-            alt="product preview"
-            width={100}
-            height={100}
-            quality={100}
-            className="animate-spin"
-          />
-        </div> */}
+        <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
+          <p className="text-sm font-semibold text-gray-700">
+            Currently in Alpha
+          </p>
+        </div>
         <h1 className="max-w-4xl text-4xl font-bold md:text-6xl lg:text-7xl">
-          Converse, Transform, Personalize: <br />
+          Converse. Transform. Personalize: <br />
           Your <span className="text-green-600">Medical</span> Data.
         </h1>
         <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
